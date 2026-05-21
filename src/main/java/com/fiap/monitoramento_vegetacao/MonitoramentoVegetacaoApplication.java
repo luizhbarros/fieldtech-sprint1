c22adf919c72464ae913828f70989be7ac9f52f0
Luiz Barros <luizh.barros1109@gmail.com>
2026-05-21 10:44:24 -0300

    feat: setup inicial do projeto Spring Boot

---
+++ src/main/java/com/fiap/monitoramento_vegetacao/MonitoramentoVegetacaoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.monitoramento_vegetacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoVegetacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoVegetacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/monitoramento_vegetacao/MonitoramentoVegetacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.monitoramento_vegetacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoVegetacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
